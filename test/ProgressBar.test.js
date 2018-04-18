fe8488a4eee3bb92d81d8f5cba58aea3457b6db0
tsaohucn <tsaohucn@gmail.com>
2018-04-18 17:56:34 +0700

    add test

--- test/ProgressBar.test.js
+++ test/ProgressBar.test.js
@@ -2,8 +2,13 @@
 import 'react-native'
 import React from 'react'
 import ProgressBar from '../CoomicBookTool/ProgressBar'
-
 import renderer from 'react-test-renderer'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
+
+import { shallow, mount, render } from 'enzyme'
 
 jest.mock('react-native-elements', () => {
 	return {
@@ -34,14 +39,30 @@ describe('Testing ProgressBar component', () => {
 	  expect(tree).toMatchSnapshot();
 	})
 
-/*
-	it('test component method', () => {
 
-	  const wrapper = renderer.create(
-	    <ProgressBar/>
-	   )
-		wrapper.instance().onValueChange()
+	it('initial state', () => {
+		const wrapper = shallow(<ProgressBar/>)
+    expect(wrapper.state('value')).toBe(0)
+	})
 
-	})*/
+	it('onValueChange', () => {
+		const wrapper = shallow(<ProgressBar/>)
+    wrapper.instance().onValueChange(20)
+		expect(wrapper.state('value')).toBe(20)
+	})
+
+	it('getChapterPageNumber', () => {
+		const chapter = [
+			{ pageNumber: 10, title: '第一章' },
+			{ pageNumber: 20, title: '第二章' },
+			{ pageNumber: 30, title: '第三章' }
+		]
+
+		const wrapper = shallow(<ProgressBar chapter={chapter}/>)
+		const instance = wrapper.instance()
+    instance.getChapterPageNumber(20)
+		expect(instance.previousChapterPageNumber).toBe(10)
+		expect(instance.nextChapterPageNumber).toBe(30)
+	})
 
 })
